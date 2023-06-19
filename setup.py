50fa1cf67ec77eb565516995571b03096d9042a8
Yeonwoo Jang <yjang385@gmail.com>
2023-06-19 00:43:06 -0400

    update setup.py to include deps

--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
 from setuptools import setup, find_packages
 
-setup(name="turing", version="0.0.1", packages=find_packages())
+setup(
+  name="turing", 
+  version="0.0.1", 
+  install_requires=[
+    "pytest",
+  ]
+  packages=find_packages()
+)
